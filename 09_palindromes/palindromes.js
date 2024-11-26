0a99853f89b090c0b7df0460317f71a55647449c
Johannes Ossanna <137186589+jossanna@users.noreply.github.com>
2024-11-26 23:58:21 +0100

    complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const cleanString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const reverse = string
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "")
+    .split("")
+    .reverse()
+    .join("");
+  return reverse === cleanString;
 };
 
 // Do not edit below this line
