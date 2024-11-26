3a0cba9acd3fef713bc8decafdee1de9b407086d
Johannes Ossanna <137186589+jossanna@users.noreply.github.com>
2024-11-27 00:38:09 +0100

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (input) {
+  if (typeof input !== "number") {
+    input = parseInt(input);
+  }
 
+  if (input === 0) {
+    return 0;
+  }
+  if (input === 1) {
+    return input;
+  }
+  if (input < 0) {
+    return "OOPS";
+  }
+
+  let fib = [0, 1];
+  for (let i = 2; i <= input; i++) {
+    fib.push(fib.at(-2) + fib.at(-1));
+  }
+  return fib.at(-1);
 };
 
 // Do not edit below this line
