954e59f7e361df0471150b995ee244568fba8c44
Johannes Ossanna <137186589+jossanna@users.noreply.github.com>
2024-11-28 00:04:21 +0100

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  return array
+    .map((person) => ({
+      ...person,
+      age: person.yearOfDeath
+        ? person.yearOfDeath - person.yearOfBirth
+        : new Date().getFullYear() - person.yearOfBirth,
+    }))
+    .sort((a, b) => b.age - a.age)[0];
 };
 
 // Do not edit below this line
